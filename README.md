25fb6823c7c11a40c8674e8bd7f5e91bfb0dc0f7
js-all <titouangoulois29@gmail.com>
2018-10-21 23:38:40 +0200

    g

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # editor
+
 ## my first editor
+
 ### **don't use it, it is bad.**
--- main.js
+++ main.js
@@ -1,35 +1,14 @@
 const EDITOR = document.getElementById('editor'),
       CODE = document.getElementById('code');
-/*
-var pos = 0;
-
-class thum {
-    constructor(before) {
-        this.before = before;
-        this.el = document.createElement('div');
-        this.el.classList.add('thum');
-        CODE.insertBefore(this.el, this.before);
-    }
-    delete() {
-        this.el.remove();
+function update() {
+    let codeAT = CODE.value;
+    let codeT = '';
+    for(let i in data.reg) {
+        const e = data.reg[i];
+        for (let j of e.words) {
+            let reg = new RegExp(e.start+j+e.end,'g');
+            
+        }
     }
 }
-var ci = CODE.innerHTML;
-
-document.addEventListener('keydown',(e) => {
-    if (e.keyCode == 8) {
-        if (ci.substring(ci.length-2,ci.length) == '\n') {
-            ci = ci.substring(0,ci.length-2);
-        } else {
-            ci = ci.substring(0, ci.length-1);
-        }
-        CODE.innerHTML = ci.replace(/\n/g,'<br />');
-    } else if (e.keyCode == 13) {
-        ci += '\n';
-        CODE.innerHTML = ci.replace(/\n/g,'<br />');
-    } else {
-        ci += String.fromCharCode(e.keyCode);
-        CODE.innerHTML = ci.replace(/\n/g,'<br />');
-    }
-});*/
 
